d2f1b03754675e0f35bc13b1b370058b58caaeaa
glULTRA <7amakurd2002@gmail.com>
2022-08-29 17:18:53 +0300

    routes under update

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FriendsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\User\ImageUploadController;
 use App\Http\Controllers\User\ProfileController;
 use App\Http\Controllers\User\UserPostController;
+use App\Http\Livewire\PostLikeCounter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
@@ -31,8 +33,8 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
-Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
+Route::post('/posts/{post}/likes', [PostLikeCounter::class, 'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeCounter::class, 'destroy'])->name('posts.likes');
 
 Route::get('/users/{user:username}/profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('/users/{user:username}/profile', [ProfileController::class, 'store']);
@@ -40,3 +42,7 @@ Route::post('/users/{user:username}/profile', [ProfileController::class, 'store'
 Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('users.posts');
 
 Route::get('/images-add', [ImageUploadController::class, 'store'])->name('images.add');
+
+Route::get('/friends', [FriendsController::class, 'index'])->name('friends');
+Route::post('/friends/{user}', [FriendsController::class, 'store'])->name('friends.add');
+
